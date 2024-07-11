685fa4b249afa275128d160ffd9a462f0fadc7de
nihaltowfiq <nihaltowfiq@gmail.com>
2024-07-11 19:30:03 +0600

    gh-pages added

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,9 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3030,
-  },
-  preview: {
-    port: 8080,
-  },
+	plugins: [react()],
+	server: {
+		port: 3000,
+	},
+	base: '/recursive-list/', // for deployment in github
 });
